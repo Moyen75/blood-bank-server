6c5b6580a2a16c90b811716680a5265a5abde8b9
Moyen75 <moyenislam75@gmail.com>
2022-01-17 01:46:16 +0600

    user data getting

--- index.js
+++ index.js
@@ -28,9 +28,42 @@ async function run() {
         // add donor to database
         app.post('/donors', async (req, res) => {
             const donor = req.body;
-            // const result=await donorsCollection.insertOne(donor)
-            // res.send(result)
+            const result = await donorsCollection.insertOne(donor)
+            res.send(result)
             console.log(donor)
+        })
+
+        // get similar data 
+        app.get('/donors', async (req, res) => {
+            const area = req.query.area;
+            const blood = req.query.blood;
+            const query = { area: area, blood: blood }
+            const filter = donorsCollection.find(query)
+            const result = await filter.toArray()
+            res.json(result)
+            console.log(JSON.stringify(query), result)
+        })
+
+        // get all donors
+        app.get('/allDonors', async (req, res) => {
+            const cursor = donorsCollection.find({})
+            const result = await cursor.toArray()
+            res.json(result)
+        })
+        // get specific blood group
+        app.get('/blood', async (req, res) => {
+            const blood = req.query.blood;
+            const query = { blood: blood }
+            const result = await donorsCollection.findOne(query)
+            res.json(result)
+        })
+
+        // get specific user
+        app.get('/user', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email }
+            const result = await usersCollection.findOne(query)
+            res.json(result)
         })
     }
     finally {
